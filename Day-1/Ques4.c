ee5a4c5db55238a5ee5793e2de15f2f10d57b87f
Manish <manishojha929@gmail.com>
2026-06-21 19:00:15 +0530

    Day-1 assignment is uploaded

---
+++ Day-1/Ques4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    long long n;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    if (n == 0) {
+        count = 1;
+    } else {
+        if (n < 0) {
+            n = -n;
+        }
+        while (n != 0) {
+            n /= 10;
+            count++;
+        }
+    }
+
+    printf("Number of digits: %d\n", count);
+
+    return 0;
+}
