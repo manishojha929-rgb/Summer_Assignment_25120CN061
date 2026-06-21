8acb1c02577dac19f36cd5c3aeea9f24a158189f
Manish <manishojha929@gmail.com>
2026-06-21 19:29:25 +0530

    assignment is uploaded

---
+++ Day-21/Q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+    int i;
+    char temp;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    for (i = 0; i < length / 2; i++) {
+        temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
